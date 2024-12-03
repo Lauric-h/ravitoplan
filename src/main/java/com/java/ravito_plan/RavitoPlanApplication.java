800cd7f6c2a9b3acedc915d0aab5c50c41a87cbd
Lauric-h <h.lauric@outlook.com>
2024-12-03 14:55:09 +0100

    First commit

---
+++ src/main/java/com/java/ravito_plan/RavitoPlanApplication.java
@@ -0,0 +1,13 @@
+package com.java.ravito_plan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RavitoPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RavitoPlanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/ravito_plan/RavitoPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.ravito_plan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RavitoPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

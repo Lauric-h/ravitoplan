edc0f7777c056dc8078320a3536c106be466039d
Lauric-h <h.lauric@outlook.com>
2024-12-03 19:06:05 +0100

    Add repository

--- src/main/java/com/java/ravito_plan/user/domain/model/User.java
+++ src/main/java/com/java/ravito_plan/user/domain/model/User.java
@@ -1,12 +1,31 @@
 package com.java.ravito_plan.user.domain.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
 }

c12d92a3d01939bd274fedd7ca52915ffa673696
Lauric-h <h.lauric@outlook.com>
2024-12-03 17:16:27 +0100

    wip user

--- src/main/java/com/java/ravito_plan/user/domain/model/User.java
+++ src/main/java/com/java/ravito_plan/user/domain/model/User.java
@@ -1,5 +1,12 @@
 package com.java.ravito_plan.user.domain.model;
 
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class User {
 
 }

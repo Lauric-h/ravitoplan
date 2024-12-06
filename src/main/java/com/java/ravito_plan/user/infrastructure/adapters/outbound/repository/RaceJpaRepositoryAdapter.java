be6a2e015066f00217f7140734ead9555a2a126b
Lauric-h <h.lauric@outlook.com>
2024-12-06 20:19:51 +0100

    wip

--- src/main/java/com/java/ravito_plan/user/infrastructure/adapters/outbound/repository/RaceJpaRepositoryAdapter.java
+++ src/main/java/com/java/ravito_plan/user/infrastructure/adapters/outbound/repository/RaceJpaRepositoryAdapter.java
@@ -1,7 +1,14 @@
 package com.java.ravito_plan.user.infrastructure.adapters.outbound.repository;
 
 import com.java.ravito_plan.race.domain.ports.outbound.RaceRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RaceJpaRepositoryAdapter implements RaceRepository {
 
+    private final ImportedRaceRepository importedRaceRepository;
+
+    public RaceJpaRepositoryAdapter(ImportedRaceRepository importedRaceRepository) {
+        this.importedRaceRepository = importedRaceRepository;
+    }
 }

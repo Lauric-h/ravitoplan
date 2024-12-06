a78997f5ce8bbc6aeee963689d3baf219712283f
Lauric-h <h.lauric@outlook.com>
2024-12-06 20:09:58 +0100

    Wip login

--- src/main/java/com/java/ravito_plan/web/controllers/auth/LoginController.java
+++ src/main/java/com/java/ravito_plan/web/controllers/auth/LoginController.java
@@ -1,12 +1,21 @@
 package com.java.ravito_plan.web.controllers.auth;
 
+import com.java.ravito_plan.user.application.dto.UserDto;
+import com.java.ravito_plan.user.application.dto.auth.LoginRequest;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
     @GetMapping("/login")
-    public String login() {
+    public String login(Model model) {
+        LoginRequest user = new LoginRequest();
+        model.addAttribute("user", user);
         return "user/login";
     }
+
 }

f1dfefbe6130a0ad16d7fd57b470684144a3c2d3
Lauric-h <h.lauric@outlook.com>
2024-12-13 18:52:19 +0100

    Transform /register to api

--- src/main/java/com/java/ravito_plan/config/SecurityConfig.java
+++ src/main/java/com/java/ravito_plan/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                    .requestMatchers("/register", "/").permitAll()
+                    .requestMatchers("/api/register", "/").permitAll()
                     .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/"))

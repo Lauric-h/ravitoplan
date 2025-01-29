c3b0cdfb031a799136ca917f96cc059c9c7bb226
Lauric-h <h.lauric@outlook.com>
2025-01-29 10:49:44 +0100

    change action

--- src/test/java/com/java/ravito_plan/RavitoPlanApplicationTests.java
+++ src/test/java/com/java/ravito_plan/RavitoPlanApplicationTests.java
@@ -3,7 +3,7 @@ package com.java.ravito_plan;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = { "spring.datasource.url= " })
 class RavitoPlanApplicationTests {
 
 	@Test
